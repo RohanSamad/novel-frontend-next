418bb967e8105bcabe4f0295c07b466ab1d77093
RohanSamad <rohansamad@proton.me>
2025-08-18 19:41:35 +0500

    added line

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -18,6 +18,8 @@ const Footer = () => {
             <p className="text-gray-300 text-sm mt-2">
               Your premier platform for audiobooks and novels.
             </p>
+            <p className="text-gray-300 text-sm mt-2">explore, read, and listen more novel audiobooks at our <Link className='text-blue-300' href="/">Homepage</Link></p>
+            
           </div>
 
           {/* Contact Button */}
