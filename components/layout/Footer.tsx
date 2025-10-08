3ef2ff061ee5a13e2463482921ba449148046292
FloatySpaghetti <rensiscolim@gmail.com>
2025-10-08 14:25:25 +0700

    Header & Footer logo_2

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,6 +1,7 @@
 'use client'
 import Link from 'next/link';
 import { Mail } from 'lucide-react';
+import BookIcon from '/public/book-icon.svg';
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
@@ -12,8 +13,8 @@ const Footer = () => {
           {/* Brand Section */}
           <div className="mb-8 md:mb-0">
             <div className="flex items-center">
-              {/* Replaced BookOpen with custom SVG */}
-              <img src="/book-icon.svg" alt="Book Icon" className="h-8 w-8" />
+              {/* Using BookIcon as a React component */}
+              <BookIcon className="h-8 w-8" />
               <span className="ml-2 text-xl font-serif font-bold">Novel Tavern</span>
             </div>
             <p className="text-gray-300 text-sm mt-2">
